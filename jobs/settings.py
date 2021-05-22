b07144984bd8f884491255ec47e5fe3e04ba5c77
Aron-R <aronritesh223@gmail.com>
2021-05-22 21:42:00 +0530

    Migrated to mysql, Removed footer text and Social media login

--- jobs/settings.py
+++ jobs/settings.py
@@ -80,8 +80,13 @@ WSGI_APPLICATION = "jobs.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        # "ENGINE": "django.db.backends.sqlite3",
+        # "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'jelly',
+        'HOST': 'localhost',
+        'USER': 'admin',
+        'PASSWORD': 'mysql123'
     },
     # 'default': {
     #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
